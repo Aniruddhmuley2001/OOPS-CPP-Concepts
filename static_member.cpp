adcce9121f8a0225f445f2a8af4d78684a4b562e
Aniruddh Muley <54072399+Aniruddhmuley2001@users.noreply.github.com>
2020-06-08 16:28:48 +0530

    Update static_member.cpp

--- static_member.cpp
+++ static_member.cpp
@@ -8,11 +8,18 @@ class Account{
     public:
         void setBalance(int b)
         { balance = b;}
+        static void setRoi(float r) //Static Member Function.
+                                    //static keyword added so to access the function without using any object.
+        { roi = r;}
 };
-float Account:: roi = 3.5f; //This line enables the static member variable to get memory space
+float Account:: roi = 3.5f; //This line enables the static member variable to get memory space.
+                            //Does not depend on the number of objects created.
 
 int main() {
 	// your code goes here
-	Account a1;
+	Account a1, a2;
+	Account::roi = 4.5f;    //Used if roi would not have been private. So not to be used here.
+	a1.setRoi(4.5f);        //Used if atleast one object available
+	Account::setRoi(4.5f);  //Used if no object to be used to access setRoi function.
 	return 0;
 }
