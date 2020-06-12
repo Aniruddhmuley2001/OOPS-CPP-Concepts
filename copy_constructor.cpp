8bf0323444ca85387e429c72ed10d24f5515df08
Aniruddh Muley <54072399+Aniruddhmuley2001@users.noreply.github.com>
2020-06-12 13:36:01 +0530

    Add files via upload

---
+++ copy_constructor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Point{
+	private:
+		int x, y;
+	public:
+		Point(int x1, int y1) {
+			x = x1;
+			y = y1;
+		}		
+		
+		//Copy constructor
+		Point(const Point &p2){
+			x = p2.x;
+			y = p2.y;
+		}
+		
+		int getX() {
+			return x;
+		}
+		
+		int getY() {
+			return y;
+		}
+};
+
+int main(){
+	Point p1(10, 15);	//Normal or here, Parameterized Constructor
+	Point p2 = p1;		//Copy Constructor
+	
+	cout << "p1.x = " << p1.getX() << ", p1.y = " << p1.getY(); 
+    cout << "\np2.x = " << p2.getX() << ", p2.y = " << p2.getY(); 
+  
+    return 0; 
+}
